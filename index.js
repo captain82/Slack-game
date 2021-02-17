09458e39ae85ec35f4c2bff8d690058bc7482dfb
captain82 <nktkumar82@gmail.com>
2021-02-17 19:40:33 +0530

    Semi colon removed

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let port = process.env.PORT || 3000;
 const importData = require("./data.json")
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+    res.send("Hello World");
+});
 
 app.get("/players", (req, res) => {
     res.send(importData);
-})
+});
 
 app.listen(port, () => {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
+});
