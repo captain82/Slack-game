d7dda0d90895585983a0a7d719df22f8754bf9e5
captain82 <nktkumar82@gmail.com>
2021-02-17 17:21:36 +0530

    Data json loading

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 let port = process.env.port || 3000;
+const importData = require("./data.json")
 
 app.get("/", (req, res) => {
     res.send("Hello World")
+})
+
+app.get("/players", (req, res) => {
+    res.send(importData);
 })
 
 app.listen(port, () => {
