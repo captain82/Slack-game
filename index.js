52851d16b35b44ac9a75ee9587f9683de8c44d10
captain82 <nktkumar82@gmail.com>
2021-02-20 21:13:12 +0530

    Testing the blocks

--- index.js
+++ index.js
@@ -71,6 +71,73 @@ slackInteractions.action('accept_tos', (payload, respond) => {
     return reply;
 });
 
+slackInteractions.action('accept_tos', (payload, respond) => {
+    console.log("hurray");
+    console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed a button`);
+
+    // Use the data model to persist the action
+    users.findBySlackId(payload.user.id)
+        .then(user => user.setPolicyAgreementAndSave(payload.actions[0].value === 'accept'))
+        .then((user) => {
+            // After the asynchronous work is done, call `respond()` with a message object to update the
+            // message.
+            let confirmation;
+            if (user.agreedToPolicy) {
+                confirmation = 'Thank you for agreeing to the terms of service';
+            } else {
+                confirmation = 'You have denied the terms of service. You will no longer have access to this app.';
+            }
+            respond(ticTacInterface);
+            //respond({ text: confirmation });
+        })
+        .catch((error) => {
+            // Handle errors
+            console.error(error);
+            respond({
+                text: 'An error occurred while recording your agreement choice.'
+            });
+        });
+
+    // Before the work completes, return a message object that is the same as the original but with
+    // the interactive elements removed.
+    const reply = payload.original_message;
+    delete reply.attachments[0].actions;
+    return reply;
+});
+
+slackInteractions.action({action_id:'4'}, (payload, respond) => {
+    console.log('Action id 4');
+
+    // Use the data model to persist the action
+    users.findBySlackId(payload.user.id)
+        .then(user => user.setPolicyAgreementAndSave(payload.actions[0].value === 'accept'))
+        .then((user) => {
+            // After the asynchronous work is done, call `respond()` with a message object to update the
+            // message.
+            let confirmation;
+            if (user.agreedToPolicy) {
+                confirmation = 'Thank you for agreeing to the terms of service';
+            } else {
+                confirmation = 'You have denied the terms of service. You will no longer have access to this app.';
+            }
+            respond(ticTacInterface2);
+            //respond({ text: confirmation });
+        })
+        .catch((error) => {
+            // Handle errors
+            console.error(error);
+            respond({
+                text: 'An error occurred while recording your agreement choice.'
+            });
+        });
+
+    // Before the work completes, return a message object that is the same as the original but with
+    // the interactive elements removed.
+    const reply = payload.original_message;
+    delete reply.attachments[0].actions;
+    return reply;
+});
+
 slackInteractions
     .options({ callbackId: 'pick_sf_neighborhood', within: 'interactive_message' }, (payload) => {
         console.log(`The user ${payload.user.name} in team ${payload.team.domain} has requested options`);
@@ -192,6 +259,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "1",
 					"value": "click_me_123"
 				},
 				{
@@ -201,6 +269,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "2",
 					"value": "click_me_123"
 				},
 				{
@@ -210,6 +279,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "3",
 					"value": "click_me_123"
 				}
 			]
@@ -224,6 +294,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "4",
 					"value": "click_me_123"
 				},
 				{
@@ -233,6 +304,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "5",
 					"value": "click_me_123"
 				},
 				{
@@ -242,6 +314,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "6",
 					"value": "click_me_123"
 				}
 			]
@@ -256,6 +329,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "7",
 					"value": "click_me_123"
 				},
 				{
@@ -265,6 +339,7 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "8",
 					"value": "click_me_123"
 				},
 				{
@@ -274,6 +349,118 @@ const ticTacInterface = {
 						"emoji": true,
 						"text": ":zap:"
 					},
+					"action_id": "9",
+					"value": "click_me_123"
+				}
+			]
+		}
+    ]
+
+}
+
+const ticTacInterface2 = {
+    "blocks":[
+        {
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "1",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "2",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "3",
+					"value": "click_me_123"
+				}
+			]
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":wave:"
+					},
+					"action_id": "4",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "5",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "6",
+					"value": "click_me_123"
+				}
+			]
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "7",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "8",
+					"value": "click_me_123"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": ":zap:"
+					},
+					"action_id": "9",
 					"value": "click_me_123"
 				}
 			]
