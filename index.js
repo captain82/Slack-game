874116bac587d63ed8ef4442b9f4110d3aba5400
captain82 <nktkumar82@gmail.com>
2021-02-20 19:47:59 +0530

    Testing the blocks

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ slackInteractions.action('accept_tos', (payload, respond) => {
             } else {
                 confirmation = 'You have denied the terms of service. You will no longer have access to this app.';
             }
-            respond.json(ticTacInterface);
+            respond(ticTacInterface);
             //respond({ text: confirmation });
         })
         .catch((error) => {
@@ -269,7 +269,7 @@ function slackSlashCommand(req, res, next) {
         console.log('entered requested')
         if (type === 'button') {
             console.log('Button requested')
-            res.json(interactiveButtons);
+            res.json(ticTacInterface);
         } else if (type === 'menu') {
             res.json(interactiveMenu);
         } else if (type === 'dialog') {
