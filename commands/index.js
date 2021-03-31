6dd0d24f529d44a966078186b9a387b7eea8e64a
captain82 <nktkumar82@gmail.com>
2021-03-31 22:46:34 +0530

    Status class created.

---
+++ commands/index.js
@@ -0,0 +1,5 @@
+module.exports.move = require('./move');
+module.exports.play = require('./play');
+module.exports.help = require('./help');
+module.exports.end = require('./end');
+module.exports.status = require('./status');
---
+++ commands/status.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const sendMessage = require('../helpers/messenger');
+const constants = require('../constants');
+
+
+function status(gameManager, channelId, res) {
+
+    if (!gameManager.hasGame(channelId)) {
+        sendMessage(res, constants.NO_GAME_EXISTS, []);
+        return;
+    }
+
+    const game = gameManager.getGame(channelId);
+    const board = game.boardToString();
+    const headlineMsg = game.getHeadLineMsg();
+    const currentPlayerMsg = game.getCurrentPlayerMsg();
+
+    sendMessage(res,headlineMsg,[board,currentPlayerMsg]);
+}
