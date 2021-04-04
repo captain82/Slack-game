a7b9c3c2385a8b8af208f3f6009513c86388a67f
captain82 <nktkumar82@gmail.com>
2021-04-04 19:55:24 +0530

    Raw body builder created

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ let gameManager;
 // and map them to their userids
 const slackClient = new WebClient(config.SLACK_API_TOKEN);
 slackClient.users.list().then((res) => {
-  for (const user of res.members) {
-    workspaceUsers[user.name] = user.id;
-  }
-  gameManager = new GameManager(workspaceUsers);
+    for (const user of res.members) {
+        workspaceUsers[user.name] = user.id;
+    }
+    gameManager = new GameManager(workspaceUsers);
 }).catch((err) => {
-  console.log(err);
+    console.log(err);
 });
 
 var app = express();
@@ -33,34 +33,35 @@ app.use(bodyParser.raw({ verify: rawBodyBuilder, type: () => true }));
 app.use(verifySlackSigningSecret);
 
 app.post('/commands', (req, res) => {
-  res.set('content-type', 'application/json');
-  const channelId = req.body.channel_id;
-  const userId = req.body.user_id;
-  const params = req.body.text.split(/[ ,]+/);
-  switch (params[0]) {
-    case 'play':
-      play(gameManager, channelId, userId, params, res);
-      break;
-    case 'status':
-      status(gameManager, channelId, res);
-      break;
-    case 'move':
-      move(gameManager, channelId, userId, params, res);
-      break;
-    case 'end':
-      end(gameManager, channelId, userId, res);
-      break;
-    case 'help':
-      help(res);
-      break;
-    default:
-      res.status(200).send(constants.INVALID_COMMAND);
-      break;
-  }
+    console.log("checking");
+    res.set('content-type', 'application/json');
+    const channelId = req.body.channel_id;
+    const userId = req.body.user_id;
+    const params = req.body.text.split(/[ ,]+/);
+    switch (params[0]) {
+        case 'play':
+            play(gameManager, channelId, userId, params, res);
+            break;
+        case 'status':
+            status(gameManager, channelId, res);
+            break;
+        case 'move':
+            move(gameManager, channelId, userId, params, res);
+            break;
+        case 'end':
+            end(gameManager, channelId, userId, res);
+            break;
+        case 'help':
+            help(res);
+            break;
+        default:
+            res.status(200).send(constants.INVALID_COMMAND);
+            break;
+    }
 });
 
 app.get('/', function (req, res) {
-  res.send(200);
+    res.send(200);
 });
 
 app.listen(process.env.PORT || 3000);
