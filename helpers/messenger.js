345462102f276c17f7cc2631af2491f5e817833e
captain82 <nktkumar82@gmail.com>
2021-04-05 22:51:33 +0530

    Zap replaced with numbers

--- helpers/messenger.js
+++ helpers/messenger.js
@@ -14,8 +14,119 @@ function _buildMessage(mainMessage,attachments){
     return{
         'response_type':'in_channel',
         'text': mainMessage,
-        'attachments':attachments
+        'attachments':ticTacInterface
     };
+}
+
+const ticTacInterface = {
+    "blocks": [
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "1",
+                    "value": "1"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "2",
+                    "value": "2"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "3",
+                    "value": "3"
+                }
+            ]
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "4",
+                    "value": "4"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "5",
+                    "value": "5"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "6",
+                    "value": "5"
+                }
+            ]
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "7",
+                    "value": "6"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "8",
+                    "value": "7"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": ":zap:"
+                    },
+                    "action_id": "9",
+                    "value": "8"
+                }
+            ]
+        }
+    ]
+
 }
 
 function sendMessage(res,mainMessage,attachmentMessages){
