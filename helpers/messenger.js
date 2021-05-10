ee6841e6fd9c0760d6b4ae0b417e8c3952f02afb
captain82 <nktkumar82@gmail.com>
2021-05-10 19:39:54 +0530

    Message added

--- helpers/messenger.js
+++ helpers/messenger.js
@@ -18,116 +18,6 @@ function _buildMessage(mainMessage,attachments){
     };
 }
 
-const ticTacInterface = {
-    "blocks": [
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "1",
-                    "value": "1"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "2",
-                    "value": "2"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "3",
-                    "value": "3"
-                }
-            ]
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "4",
-                    "value": "4"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "5",
-                    "value": "5"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "6",
-                    "value": "5"
-                }
-            ]
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "7",
-                    "value": "6"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":zap:"
-                    },
-                    "action_id": "8",
-                    "value": "7"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": ":wave:"
-                    },
-                    "action_id": "9",
-                    "value": "8"
-                }
-            ]
-        }
-    ]
-}
-
 function sendMessage(res,mainMessage,attachmentMessages){
     const attachments = _buildAttachments(attachmentMessages);
     const message = _buildMessage(mainMessage,attachments);
@@ -135,7 +25,7 @@ function sendMessage(res,mainMessage,attachmentMessages){
 }
 
 function sendJsonMessage(res,json){
-    res(json);
+    res.status(200).json(json);
 }
 
 module.exports = sendMessage;
